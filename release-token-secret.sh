a6cab6d07651a4e54bd2edb93ad01d61214f5e94
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-07-18 15:49:05 +0800

    Update release-token-secret.sh

--- release-token-secret.sh
+++ release-token-secret.sh
@@ -26,7 +26,7 @@ generate_access_keys() {
 # Function to confirm user choice
 confirm_user() {
     read -p "Is '$1' the correct user? Enter 'yes' to generate access key or any other key to skip: " confirmation
-    if [ "$confirmation = "yes" ]; then
+    if [ "$confirmation" == "yes" ]; then
         return 1
     else
         echo "Exit: $1"
@@ -60,7 +60,7 @@ main() {
             while read -r user; do
                 # Confirm if this is the correct user
                 if confirm_user $user; then
-                    generate_access_keys $user
+                    generate_access_keys '$user'
                 else
                     echo "Skipping user '$user'."
                 fi
